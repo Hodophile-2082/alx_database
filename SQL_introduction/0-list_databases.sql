1c59c97a215c0b3c5b11d49c6d3527cabcafaa4c
Hodophile-2082 <jeanwanjira2802@gmail.com>
2023-11-04 23:10:13 +0300

    Learning SQL

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,6 +1,2 @@
--- MySQL login credentials
-MYSQL_USER="your_username"
-MYSQL_PASSWORD="your_password"
-
 --Execute MySQL command to list databases
 mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "SHOW DATABASES;"
